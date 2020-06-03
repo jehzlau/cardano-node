8e5ed92cb28a33324801f1b3fdaa7610ca1cde36
Jordan Millar <jordan.millar@iohk.io>
2020-06-03 16:42:53 +0100

    Add convert-itn-key command to cardano-cli

--- cardano-api/src/Cardano/Api/Convert.hs
+++ cardano-api/src/Cardano/Api/Convert.hs
@@ -1,19 +1,28 @@
 module Cardano.Api.Convert
-  ( addressFromHex
+  ( ConversionError
+  , addressFromHex
   , addressToHex
+  , convertITNpublicKey
+  , convertITNsigningKey
   , parseTxIn
   , parseTxOut
+  , readText
+  , renderConversionError
   , renderTxIn
   , renderTxOut
   ) where
 
+import           Cardano.Api.Error (textShow)
 import           Cardano.Api.Types
 import qualified Cardano.Binary as Binary
 import           Cardano.Prelude
 import           Prelude (String)
 
+import           Control.Exception (IOException)
+import qualified Control.Exception as Exception
 import           Control.Monad.Fail (fail)
 
+import qualified Codec.Binary.Bech32 as Bech32
 import           Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8 as Atto
 import qualified Data.ByteString.Base16 as Base16
@@ -22,11 +31,30 @@ import           Data.Char (isAlphaNum)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 
+import qualified Cardano.Crypto.DSIGN as DSIGN
 import qualified Cardano.Crypto.Hash.Class as Crypto
 import qualified Cardano.Crypto.Hash.Blake2b as Crypto
 
 import qualified Shelley.Spec.Ledger.Address as Shelley
+import qualified Shelley.Spec.Ledger.Keys as Shelley
 
+data ConversionError
+  = Bech32DecodingError !FilePath !Bech32.DecodingError
+  | ITNErr !Text
+  | SigningKeyDeserializationError !ByteString
+  | VerificationKeyDeserializationError !ByteString
+  deriving Show
+
+renderConversionError :: ConversionError -> Text
+renderConversionError err =
+  case err of
+    Bech32DecodingError fp decErr ->
+      "Error decoding Bech32 key at:" <> textShow fp <> " Error: " <> textShow decErr
+    ITNErr errMessage -> errMessage
+    SigningKeyDeserializationError sKey ->
+      "Error deserialising signing key: " <> textShow (C8.unpack sKey)
+    VerificationKeyDeserializationError vKey ->
+      "Error deserialising verification key: " <> textShow (C8.unpack vKey)
 
 addressFromHex :: Text -> Maybe Address
 addressFromHex txt =
@@ -102,3 +130,41 @@ pAddress =
 
 pAlphaNumToByteString :: Parser ByteString
 pAlphaNumToByteString = Atto.takeWhile1 isAlphaNum
+
+
+-- | Convert public ed25519 key to a Shelley stake verification key
+convertITNpublicKey :: Text -> Either ConversionError StakingVerificationKey
+convertITNpublicKey pubKey = do
+  keyBS <- decodeBech32Key pubKey
+  case DSIGN.rawDeserialiseVerKeyDSIGN keyBS of
+    Just verKey -> Right . StakingVerificationKeyShelley $ Shelley.VKey verKey
+    Nothing -> Left $ VerificationKeyDeserializationError keyBS
+
+-- | Convert private ed22519 key to a Shelley signing key.
+convertITNsigningKey :: Text -> Either ConversionError SigningKey
+convertITNsigningKey privKey = do
+  keyBS <- decodeBech32Key privKey
+  case DSIGN.rawDeserialiseSignKeyDSIGN keyBS of
+    Just signKey -> Right $ SigningKeyShelley signKey
+    Nothing -> Left $ SigningKeyDeserializationError keyBS
+
+-- | Convert ITN Bech32 public or private keys to 'ByteString's
+decodeBech32Key :: Text -> Either ConversionError ByteString
+decodeBech32Key key =
+  case Bech32.decode key of
+    Left err -> Left . ITNErr $ textShow err
+    Right (_, dataPart) -> case Bech32.dataPartToBytes dataPart of
+                             Nothing -> Left $ ITNErr "Error extracting a ByteString from a DataPart: \
+                                                      \See bech32 library function: dataPartToBytes"
+                             Just bs -> Right bs
+
+readText :: FilePath -> IO (Either Text Text)
+readText fp = do
+  eStr <- Exception.try $ readFile fp
+  case eStr of
+    Left e -> return . Left $ handler e
+    Right txt -> return $ Right txt
+ where
+  handler :: IOException -> Text
+  handler e = Text.pack $ "Cardano.Api.Convert.readText: "
+                     ++ displayException e
